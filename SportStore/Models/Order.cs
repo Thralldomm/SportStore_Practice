8ad0fee68461e612c1ce2ff7749da67b3267df96
CollegeSKS <aminasemenova033@gmail.com>
2024-04-20 19:28:29 +0300

    Подключение к БД

---
+++ SportStore/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SportStore.Models;
+
+public partial class Order
+{
+    public int Id { get; set; }
+
+    public string OrderList { get; set; } = null!;
+
+    public DateOnly OrderDate { get; set; }
+
+    public DateOnly DeliveryDate { get; set; }
+
+    public int PickupPointId { get; set; }
+
+    public string Client { get; set; } = null!;
+
+    public string Code { get; set; } = null!;
+
+    public string Status { get; set; } = null!;
+
+    public virtual ICollection<OrderProduct> OrderProducts { get; set; } = new List<OrderProduct>();
+}
---
+++ SportStore/Models/SportStoreContext.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportStore.Models;
+
+public partial class SportStoreContext : DbContext
+{
+    public SportStoreContext()
+    {
+    }
+
+    public SportStoreContext(DbContextOptions<SportStoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderProduct> OrderProducts { get; set; }
+
+    public virtual DbSet<PickupPoint> PickupPoints { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-SLCU6RB\\SQLEXPRESS;Initial Catalog=SportStore;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tmp_ms_x__3214EC074161E928");
+
+            entity.ToTable("Order");
+        });
+
+        modelBuilder.Entity<OrderProduct>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__OrderPro__3214EC077961B947");
+
+            entity.ToTable("OrderProduct");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderProducts)
+                .HasForeignKey(d => d.OrderId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__OrderProd__Order__49C3F6B7");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.OrderProducts)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__OrderProd__Produ__70DDC3D8");
+        });
+
+        modelBuilder.Entity<PickupPoint>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__PickupPo__3214EC07B976E0A2");
+
+            entity.ToTable("PickupPoint");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tmp_ms_x__3214EC071B05CC44");
+
+            entity.ToTable("Product");
+
+            entity.HasIndex(e => e.ArticleNumber, "UQ__tmp_ms_x__3C99114297C6449D").IsUnique();
+
+            entity.Property(e => e.ArticleNumber).HasMaxLength(100);
+            entity.Property(e => e.Cost).HasColumnType("decimal(19, 4)");
+            entity.Property(e => e.Photo).IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Role__3214EC077BBEF2F4");
+
+            entity.ToTable("Role");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__User__3214EC078936891C");
+
+            entity.ToTable("User");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Patronymic).HasMaxLength(100);
+            entity.Property(e => e.Surname).HasMaxLength(100);
+
+            entity.HasOne(d => d.RoleNavigation).WithMany(p => p.Users)
+                .HasForeignKey(d => d.Role)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__User__Role__267ABA7A");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
